ad4a17cfa65ba99a137d5a5f363bebdddcebbae0
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-04-25 22:06:47 +0200

    JavaFX added to start application

--- src/main/java/database/manager/Main.java
+++ src/main/java/database/manager/Main.java
@@ -1,10 +1,30 @@
-package dataBase;
+package database.manager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.sql.*;
 import java.util.*;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader();
+        Pane pane = loader.load(getClass().getResource("/fxml/main.fxml"));
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        launch(args);
 
         String check= "go";
         String exit ="exit";
