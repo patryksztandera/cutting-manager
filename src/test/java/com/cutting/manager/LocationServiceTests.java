b915150b2254a8c5d162a073c93422882b7fdf30
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-09-11 23:01:35 +0200

    Test delete method

---
+++ src/test/java/com/cutting/manager/LocationServiceTests.java
@@ -0,0 +1,33 @@
+package com.cutting.manager;
+
+import com.cutting.manager.models.repositories.LocationRepository;
+import com.cutting.manager.models.responses.LocationFxModel;
+import com.cutting.manager.models.services.LocationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("h2")
+public class LocationServiceTests {
+
+    @Autowired
+    private LocationService locationService;
+
+    @Autowired
+    private LocationRepository repository;
+
+    @Test
+    void deleteByLocation() {
+        final LocationFxModel model = new LocationFxModel("location");
+
+        assertEquals(0,repository.count());
+        locationService.add(model);
+        assertEquals(1,repository.count());
+        locationService.delete(model.getLocation());
+        assertEquals(0,repository.count());
+    }
+}
