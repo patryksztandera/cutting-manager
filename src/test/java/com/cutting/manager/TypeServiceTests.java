57364313103c6837d73e5fc083bb2364a26f7dc3
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-09-11 21:55:28 +0200

    Test of delete method

---
+++ src/test/java/com/cutting/manager/TypeServiceTests.java
@@ -0,0 +1,33 @@
+package com.cutting.manager;
+
+import com.cutting.manager.models.repositories.TypeRepository;
+import com.cutting.manager.models.responses.TypeFxModel;
+import com.cutting.manager.models.services.TypeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("h2")
+public class TypeServiceTests {
+
+    @Autowired
+    private TypeService typeService;
+
+    @Autowired
+    private TypeRepository repository;
+
+    @Test
+    void deleteTypeModel() {
+        final TypeFxModel model = new TypeFxModel("type","info");
+
+        assertEquals(0,repository.count());
+        typeService.add(model);
+        assertEquals(1,repository.count());
+        typeService.delete(model.getType());
+        assertEquals(0,repository.count());
+    }
+}
