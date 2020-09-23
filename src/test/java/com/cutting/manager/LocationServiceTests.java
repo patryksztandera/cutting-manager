98390c3fbcbda165324edeea8a40354ab62a4084
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-09-24 01:00:08 +0200

    Test update method

--- src/test/java/com/cutting/manager/LocationServiceTests.java
+++ src/test/java/com/cutting/manager/LocationServiceTests.java
@@ -3,6 +3,7 @@ package com.cutting.manager;
 import com.cutting.manager.models.repositories.LocationRepository;
 import com.cutting.manager.models.responses.LocationFxModel;
 import com.cutting.manager.models.services.LocationService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,11 @@ public class LocationServiceTests {
     @Autowired
     private LocationRepository repository;
 
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
     @Test
     void deleteByLocation() {
         final LocationFxModel model = new LocationFxModel("location");
@@ -29,5 +35,16 @@ public class LocationServiceTests {
         assertEquals(1,repository.count());
         locationService.delete(model.getLocation());
         assertEquals(0,repository.count());
+    }
+
+    @Test
+    void updateByLocation() {
+        final LocationFxModel model = new LocationFxModel("location");
+
+        assertEquals(0,repository.count());
+        locationService.add(model);
+        assertEquals(1,repository.count());
+        locationService.updateLocation("location","new");
+        assertEquals("new", locationService.getLocations().get(0));
     }
 }
