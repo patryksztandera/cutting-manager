51e7cb850c5d9d26576736a91aa7270f2a9d42b4
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-09-27 12:58:21 +0200

    Create getById method

--- src/main/java/com/cutting/manager/models/repositories/MetalSheetRepository.java
+++ src/main/java/com/cutting/manager/models/repositories/MetalSheetRepository.java
@@ -4,9 +4,7 @@ import com.cutting.manager.models.entities.MetalSheetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.ZonedDateTime;
-
 @Repository
 public interface MetalSheetRepository extends JpaRepository<MetalSheetEntity, Long> {
-    void deleteByTimestamp(ZonedDateTime timestamp);
+    MetalSheetEntity getById(Long id);
 }
