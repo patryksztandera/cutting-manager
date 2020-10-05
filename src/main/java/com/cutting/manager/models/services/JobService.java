f59b3198c2f77a1dcee6414bf6de95d610ff123d
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-10-06 01:15:33 +0200

    Create methods add and getAll

--- src/main/java/com/cutting/manager/models/services/JobService.java
+++ src/main/java/com/cutting/manager/models/services/JobService.java
@@ -1,4 +1,39 @@
 package com.cutting.manager.models.services;
 
+import com.cutting.manager.models.entities.JobEntity;
+import com.cutting.manager.models.repositories.JobRepository;
+import com.cutting.manager.models.repositories.MetalSheetRepository;
+import com.cutting.manager.models.responses.JobFxModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JobService {
+    private final JobRepository jobRepository;
+    private final MetalSheetRepository metalSheetRepository;
+
+    public JobService(JobRepository jobRepository, MetalSheetRepository metalSheetRepository) {
+        this.jobRepository = jobRepository;
+        this.metalSheetRepository = metalSheetRepository;
+    }
+
+    public ObservableList<JobFxModel> getAll() {
+        ObservableList<JobFxModel> jobFxModelObservableList = FXCollections.observableArrayList();
+
+        jobRepository.findAll().forEach(e -> jobFxModelObservableList.add(new JobFxModel(e)));
+        return jobFxModelObservableList;
+    }
+
+    public void add(JobFxModel model) {
+        jobRepository.save(mapFxModel(model));
+    }
+
+    private JobEntity mapFxModel(JobFxModel model) {
+        return new JobEntity(
+                model.getName(),
+                model.getFileType(),
+                model.getImgByte(),
+                metalSheetRepository.getById(model.getMetalSheetId()));
+    }
 }
