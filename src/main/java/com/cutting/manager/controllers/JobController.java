51128e8d0257b7a803d5d7391f83225fc6e46803
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-10-17 18:35:17 +0200

    Use of File Chooser and add bindings

--- src/main/java/com/cutting/manager/controllers/JobController.java
+++ src/main/java/com/cutting/manager/controllers/JobController.java
@@ -1,85 +1,116 @@
 package com.cutting.manager.controllers;
 
 import com.cutting.manager.models.responses.JobFxModel;
+import com.cutting.manager.models.responses.MetalSheetFxModel;
 import com.cutting.manager.models.services.JobService;
+import com.cutting.manager.models.services.MetalSheetService;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
 @Component
 public class JobController {
     private final JobService jobService;
+    private final MetalSheetService metalSheetService;
     @FXML
     public TextField nameTextField;
     @FXML
-    public TextField pathTextField;
-    @FXML
-    public TextField metalSheetIdTextField;
-    @FXML
     public Button addButton;
     @FXML
-    public TableView<JobFxModel> jobTable;
+    public Button fileChooserButton;
     @FXML
-    public TableColumn<JobFxModel, String> nameColumn;
+    public TextField pathTextField;
     @FXML
-    public TableColumn<JobFxModel, String> postTimeColumn;
+    public Label metalSheetLabel;
     @FXML
-    public TableColumn<JobFxModel, String> endTimeColumn;
+    public Button selectMetalSheetButton;
     @FXML
-    public ImageView imageView;
+    public TableView<MetalSheetFxModel> metalSheetTable;
+    @FXML
+    public TableColumn<MetalSheetFxModel, Number> lengthColumn;
+    @FXML
+    public TableColumn<MetalSheetFxModel, Number> widthColumn;
+    @FXML
+    public TableColumn<MetalSheetFxModel, Number> thicknessColumn;
+    @FXML
+    public TableColumn<MetalSheetFxModel, String> typeColumn;
+    @FXML
+    public TableColumn<MetalSheetFxModel, String> ownerColumn;
+    public CheckBox checkSheet;
 
-    public JobController(JobService jobService) {
+    MetalSheetFxModel model;
+    File file;
+
+    public JobController(JobService jobService, MetalSheetService metalSheetService) {
         this.jobService = jobService;
+        this.metalSheetService = metalSheetService;
     }
 
     public void initialize() {
         addBindings();
         tableSettings();
-        imageViewSettings();
     }
 
     private void addBindings() {
+        this.checkSheet.disableProperty().set(true);
         this.addButton.disableProperty().bind(nameTextField.textProperty().isEmpty()
-                .or(pathTextField.textProperty().isEmpty())
-                .or(metalSheetIdTextField.textProperty().isEmpty()));
+                .or(pathTextField.textProperty().isEmpty()).or(this.checkSheet.selectedProperty().not()));
     }
 
     private void tableSettings() {
-        this.jobTable.setItems(jobService.getAll());
-        this.nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-        this.postTimeColumn.setCellValueFactory(cellData -> cellData.getValue().postTimeProperty());
-        this.endTimeColumn.setCellValueFactory(cellData -> cellData.getValue().endTimeProperty());
+        this.metalSheetTable.setItems(metalSheetService.getAll());
+        this.lengthColumn.setCellValueFactory(cellData -> cellData.getValue().lengthProperty());
+        this.widthColumn.setCellValueFactory(cellData -> cellData.getValue().widthProperty());
+        this.thicknessColumn.setCellValueFactory(cellData -> cellData.getValue().thicknessProperty());
+        this.typeColumn.setCellValueFactory(cellData -> cellData.getValue().typeProperty());
+        this.ownerColumn.setCellValueFactory(cellData -> cellData.getValue().ownerProperty());
     }
 
-    private void imageViewSettings() {
-        this.jobTable.getSelectionModel().selectedItemProperty().addListener(((observable, oldValue, newValue) -> {
-            Image image = new Image(new ByteArrayInputStream(observable.getValue().getImgByte()));
-            this.imageView.setImage(image);
-        }));
+    public void fileOnAction() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image", "*.jpg", "*.jpeg", "*.png"));
+        this.file = fileChooser.showOpenDialog(null);
+        pathTextField.setText(this.file.getAbsolutePath());
+    }
+
+    public void selectOnAction() {
+        this.model = metalSheetTable.getSelectionModel().getSelectedItem();
+        StringBuilder stringBuilder = new StringBuilder("Dimensions: " +
+                model.getLength() + " x " + model.getWidth() + " x " + model.getThickness()
+                + ", - " + model.getType() + " -");
+        if (model.getOwner() != null)
+            stringBuilder.append(" (").append(model.getOwner()).append(")");
+        metalSheetLabel.setText(stringBuilder.toString());
+        setCheckBox(true);
     }
 
     public void addJob() {
-        File file = new File(this.pathTextField.getText());
         try {
             jobService.add(new JobFxModel(
                     this.nameTextField.getText(),
                     FilenameUtils.getExtension(this.pathTextField.getText()),
-                    Long.parseLong(this.metalSheetIdTextField.getText()),
-                    FileUtils.readFileToByteArray(file)));
+                    this.model.getId(),
+                    FileUtils.readFileToByteArray(this.file)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        this.nameTextField.clear();
+        this.pathTextField.clear();
+        this.metalSheetLabel.setText("");
+        setCheckBox(false);
         initialize();
+    }
+
+    private void setCheckBox(boolean b) {
+        this.checkSheet.disableProperty().set(false);
+        this.checkSheet.setSelected(b);
+        this.checkSheet.disableProperty().set(true);
     }
 }
