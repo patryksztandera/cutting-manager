217be9acd42e222bdc5cdc4f39f180b1750f29cb
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-10-20 11:21:25 +0200

    Create layout and changing job status to done

--- src/main/java/com/cutting/manager/controllers/JobViewerController.java
+++ src/main/java/com/cutting/manager/controllers/JobViewerController.java
@@ -1,6 +1,10 @@
 package com.cutting.manager.controllers;
 
 import com.cutting.manager.models.responses.JobFxModel;
+import com.cutting.manager.models.responses.MetalSheetFxModel;
+import com.cutting.manager.models.services.JobService;
+import com.cutting.manager.models.services.MetalSheetService;
+import com.cutting.manager.models.services.TypeService;
 import com.cutting.manager.singleton.JobSingleton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,6 +13,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -28,15 +33,41 @@ public class JobViewerController {
     @Value("classpath:/fxml/jobList.fxml")
     private Resource jobListResource;
     private final ApplicationContext applicationContext;
+    private final JobService jobService;
+    private final MetalSheetService metalSheetService;
+    private final TypeService typeService;
     @FXML
     public Button backButton;
     @FXML
+    public Button doneButton;
+    @FXML
     public ImageView imageView;
+    @FXML
+    public Label name;
+    @FXML
+    public Label length;
+    @FXML
+    public Label width;
+    @FXML
+    public Label thickness;
+    @FXML
+    public Label type;
+    @FXML
+    public Label info;
+    @FXML
+    public Label locationLabel;
+    @FXML
+    public Label quantityLabel;
+
 
     JobFxModel jobFxModel;
+    MetalSheetFxModel metalSheetFxModel;
 
-    public JobViewerController(ApplicationContext applicationContext) {
+    public JobViewerController(ApplicationContext applicationContext, JobService jobService, MetalSheetService metalSheetService, TypeService typeService) {
         this.applicationContext = applicationContext;
+        this.jobService = jobService;
+        this.metalSheetService = metalSheetService;
+        this.typeService = typeService;
     }
 
     public void initialize() {
@@ -45,6 +76,28 @@ public class JobViewerController {
 
         Image image = new Image(new ByteArrayInputStream(this.jobFxModel.getImgByte()));
         this.imageView.setImage(image);
+
+        metalSheetFxModel = metalSheetService.getOne(this.jobFxModel.getMetalSheetId());
+        labelsSettings();
+        bindings();
+    }
+
+    private void bindings() {
+        doneButton.disableProperty().set(true);
+        if (metalSheetFxModel.getQuantity() > 0) {
+            doneButton.disableProperty().set(false);
+        }
+    }
+
+    private void labelsSettings() {
+        this.name.setText(this.jobFxModel.getName());
+        this.length.setText("Length: "+metalSheetFxModel.getLength());
+        this.width.setText("Width: "+metalSheetFxModel.getWidth());
+        this.thickness.setText("Thickness: "+metalSheetFxModel.getThickness());
+        this.type.setText(metalSheetFxModel.getType());
+        this.info.setText(typeService.getByType(metalSheetFxModel.getType()).getInfo() );
+        this.locationLabel.setText(metalSheetFxModel.getLocation());
+        this.quantityLabel.setText(String.valueOf(metalSheetFxModel.getQuantity()));
     }
 
     public void backToPreviousStage(ActionEvent actionEvent) throws IOException {
@@ -62,5 +115,11 @@ public class JobViewerController {
         FXMLLoader fxmlLoader = new FXMLLoader(resource.getURL());
         fxmlLoader.setControllerFactory(applicationContext::getBean);
         return fxmlLoader.load();
+    }
+
+    public void doneOnAction() {
+        jobService.setEndTime(jobFxModel.getId());
+        metalSheetService.updateQuantity(metalSheetFxModel.getId(), metalSheetFxModel.getQuantity() - 1);
+        initialize();
     }
 }
