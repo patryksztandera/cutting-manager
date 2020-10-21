8c8481604f53f30bb3ff88ca0291a4c9c13cef2b
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-10-21 23:19:12 +0200

    Create service for client with add and get all methods

---
+++ src/main/java/com/cutting/manager/models/services/ClientService.java
@@ -0,0 +1,36 @@
+package com.cutting.manager.models.services;
+
+import com.cutting.manager.models.entities.ClientEntity;
+import com.cutting.manager.models.repositories.ClientRepository;
+import com.cutting.manager.models.responses.ClientFxModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public ObservableList<ClientFxModel> getAll() {
+        ObservableList<ClientFxModel> clientFxModelObservableList = FXCollections.observableArrayList();
+
+        clientRepository.findAll().forEach(e -> clientFxModelObservableList.add(new ClientFxModel(e)));
+        return clientFxModelObservableList;
+    }
+
+    public void add(ClientFxModel model) {
+        clientRepository.save(mapFxModel(model));
+    }
+
+    private ClientEntity mapFxModel(ClientFxModel model) {
+        return new ClientEntity(
+                model.getName(),
+                model.getSurname(),
+                model.getEmail(),
+                model.getPassword());
+    }
+}
