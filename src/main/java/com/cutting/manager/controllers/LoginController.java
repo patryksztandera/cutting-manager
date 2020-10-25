6d7094695fe9ec702b471aa2ee70ebcc05506177
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-10-25 20:02:12 +0100

    Basic layout with BCrypt for password checking

--- src/main/java/com/cutting/manager/controllers/LoginController.java
+++ src/main/java/com/cutting/manager/controllers/LoginController.java
@@ -1,4 +1,66 @@
 package com.cutting.manager.controllers;
 
-public class ClientController {
+import com.cutting.manager.models.services.ClientService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoginController {
+    private final ClientService clientService;
+    private final ApplicationContext applicationContext;
+    @Value("classpath:/fxml/manager.fxml")
+    private Resource managerResource;
+    @Value("classpath:/fxml/register.fxml")
+    private Resource registerResource;
+    @FXML
+    public TextField emailTextField;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public Button loginButton;
+    @FXML
+    public Button registerButton;
+
+    public LoginController(ClientService clientService, ApplicationContext applicationContext) {
+        this.clientService = clientService;
+        this.applicationContext = applicationContext;
+    }
+
+    public void initialize() {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        if (BCrypt.checkpw(this.passwordField.getText(), clientService.getByEmail(this.emailTextField.getText()).getPassword())) {
+            loadStage(actionEvent, managerResource);
+        }
+    }
+
+    public void registerAction(ActionEvent actionEvent) throws IOException {
+        loadStage(actionEvent, registerResource);
+    }
+
+    private void loadStage(ActionEvent actionEvent, Resource resource) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(resource.getURL());
+        fxmlLoader.setControllerFactory(applicationContext::getBean);
+        Scene managerScene = new Scene(fxmlLoader.load());
+
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(managerScene);
+        window.show();
+    }
 }
