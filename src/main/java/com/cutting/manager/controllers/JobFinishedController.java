13c0ef7de62214a0aeb3f13f34d563256b7997d7
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-10-28 01:08:48 +0100

    Show only important info in table

--- src/main/java/com/cutting/manager/controllers/JobFinishedController.java
+++ src/main/java/com/cutting/manager/controllers/JobFinishedController.java
@@ -2,7 +2,6 @@ package com.cutting.manager.controllers;
 
 import com.cutting.manager.models.responses.JobFxModel;
 import com.cutting.manager.models.services.JobService;
-import com.cutting.manager.models.services.MetalSheetService;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobFinishedController {
     private final JobService jobService;
-    private final MetalSheetService metalSheetService;
     @FXML
     public TableView<JobFxModel> jobTable;
     @FXML
@@ -20,18 +18,9 @@ public class JobFinishedController {
     public TableColumn<JobFxModel, String> postTimeColumn;
     @FXML
     public TableColumn<JobFxModel, String> doneColumn;
-    @FXML
-    public TableColumn<JobFxModel, String> lengthColumn;
-    @FXML
-    public TableColumn<JobFxModel, String> widthColumn;
-    @FXML
-    public TableColumn<JobFxModel, String> thicknessColumn;
-    @FXML
-    public TableColumn<JobFxModel, String> typeColumn;
 
-    public JobFinishedController(JobService jobService, MetalSheetService metalSheetService) {
+    public JobFinishedController(JobService jobService) {
         this.jobService = jobService;
-        this.metalSheetService = metalSheetService;
     }
 
     public void initialize() {
@@ -43,13 +32,5 @@ public class JobFinishedController {
         this.nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
         this.postTimeColumn.setCellValueFactory(cellData -> cellData.getValue().postTimeProperty());
         this.doneColumn.setCellValueFactory(cellData -> cellData.getValue().endTimeProperty());
-        this.lengthColumn.setCellValueFactory(cellData -> metalSheetService
-                .getOne(cellData.getValue().getMetalSheetId()).lengthProperty().asString());
-        this.widthColumn.setCellValueFactory(cellData -> metalSheetService
-                .getOne(cellData.getValue().getMetalSheetId()).widthProperty().asString());
-        this.thicknessColumn.setCellValueFactory(cellData -> metalSheetService
-                .getOne(cellData.getValue().getMetalSheetId()).thicknessProperty().asString());
-        this.typeColumn.setCellValueFactory(cellData -> metalSheetService
-                .getOne(cellData.getValue().getMetalSheetId()).typeProperty());
     }
 }
