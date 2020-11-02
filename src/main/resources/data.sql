ebac3d4f95d8d7668dc02ba0fe38ba423726fc29
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-11-02 23:34:31 +0100

    Populate databese with default data and crate root user with admin role

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO clients (id, name, surname, email, password, admin_role)
+VALUES ('1', 'admin', 'ADMIN', 'admin@mail.com', '$2a$10$v41LTDo9HsBzfQiMIv7Is.VITHrdS1vG23vhAbkqkh557YAPBJMbW', TRUE);
